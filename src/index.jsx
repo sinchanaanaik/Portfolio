b7cf554ae646d6d8b030bd21101fbfeb35d378a6
Sinchanaanaik <yourgithubemail@example.com>
2025-12-18 14:57:04 +0530

    Fix blank page: add ThemeProvider with darkTheme

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import MainApp from './MainApp';
+import { darkTheme } from './theme/themes'; // 👈 correct path
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <HashRouter>
-    <MainApp />
-  </HashRouter>,
+  <ThemeProvider theme={{ ...darkTheme, darkMode: true }}>
+    <HashRouter>
+      <MainApp />
+    </HashRouter>
+  </ThemeProvider>,
 );
